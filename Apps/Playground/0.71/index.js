135765fcf23bba222c700a050a99c4682b2daf9e
hieuduong <>
2024-12-26 09:42:16 +0700

    test okay on iphone nuvixe - render first successfull only once when init metro first

--- Apps/Playground/0.71/index.js
+++ Apps/Playground/0.71/index.js
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
-import App from '../playground-shared/App';
+// import App from '../playground-shared/App';
+import App from './AppNe';
 import {name as appName} from '../playground-shared/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- Apps/Playground/0.71/metro.config.js
+++ Apps/Playground/0.71/metro.config.js
@@ -7,6 +7,7 @@
 const path = require('path');
 const fs = require('fs');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
 // Escape function taken from the MDN documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
 function escapeRegExp(string) {
@@ -34,7 +35,7 @@ function processModuleSymLinks() {
         }
         const linkStat = fs.statSync(linkPath);
         if (linkStat.isDirectory()) {
-          const packagePath = path.resolve(linkPath, "package.json");
+          const packagePath = path.resolve(linkPath, 'package.json');
           if (fs.existsSync(packagePath)) {
             const packageId = path.relative(nodeModulesPath, itemPath);
             moduleMappings[packageId] = linkPath;
@@ -42,14 +43,28 @@ function processModuleSymLinks() {
             const packageInfoData = fs.readFileSync(packagePath);
             const packageInfo = JSON.parse(packageInfoData);
 
-            const dependencies = packageInfo.dependencies ? Object.keys(packageInfo.dependencies) : [];
-            const peerDependencies = packageInfo.peerDependencies ? Object.keys(packageInfo.peerDependencies) : [];
-            const devDependencies = packageInfo.devDependencies ? Object.keys(packageInfo.devDependencies) : [];
+            const dependencies = packageInfo.dependencies
+              ? Object.keys(packageInfo.dependencies)
+              : [];
+            const peerDependencies = packageInfo.peerDependencies
+              ? Object.keys(packageInfo.peerDependencies)
+              : [];
+            const devDependencies = packageInfo.devDependencies
+              ? Object.keys(packageInfo.devDependencies)
+              : [];
 
             // Exclude dependencies that appear in devDependencies or peerDependencies but not in dependencies. Otherwise,
             // the metro bundler will package those devDependencies/peerDependencies as unintended copies.
-            for (const devDependency of devDependencies.concat(peerDependencies).filter(dependency => !dependencies.includes(dependency))) {
-              moduleExclusions.push(new RegExp(escapeRegExp(path.join(linkPath, "node_modules", devDependency)) + "\/.*"));
+            for (const devDependency of devDependencies
+              .concat(peerDependencies)
+              .filter(dependency => !dependencies.includes(dependency))) {
+              moduleExclusions.push(
+                new RegExp(
+                  escapeRegExp(
+                    path.join(linkPath, 'node_modules', devDependency),
+                  ) + '/.*',
+                ),
+              );
             }
           }
         }
@@ -65,8 +80,10 @@ function processModuleSymLinks() {
 }
 
 const [moduleMappings, moduleExclusions] = processModuleSymLinks();
-console.log("Mapping the following sym linked packages:");
+console.log('Mapping the following sym linked packages:');
 console.log(moduleMappings);
+const defaultConfig = getDefaultConfig(__dirname);
+
 
 module.exports = {
   transformer: {
@@ -79,6 +96,8 @@ module.exports = {
   },
 
   resolver: {
+    // Extend asset extensions to include .gltf and .glb
+    assetExts: [...defaultConfig.resolver.assetExts, 'gltf', 'glb'],
     // Register an "extra modules proxy" for resolving modules outside of the normal resolution logic.
     extraNodeModules: new Proxy(
       // Provide the set of known local package mappings.
@@ -86,19 +105,24 @@ module.exports = {
       {
         // Provide a mapper function, which uses the above mappings for associated package ids,
         // otherwise fall back to the standard behavior and just look in the node_modules directory.
-        get: (target, name) => name in target ? target[name] : path.join(__dirname, `node_modules/${name}`),
+        get: (target, name) =>
+          name in target
+            ? target[name]
+            : path.join(__dirname, `node_modules/${name}`),
       },
     ),
-    
-    blockList: exclusionList(moduleExclusions.concat([
-      // Avoid error EBUSY: resource busy or locked, open 'D:\a\1\s\packages\playground\msbuild.ProjectImports.zip' in pipeline
-      /.*\.ProjectImports\.zip/,
-  
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
-    ])),
+
+    blockList: exclusionList(
+      moduleExclusions.concat([
+        // Avoid error EBUSY: resource busy or locked, open 'D:\a\1\s\packages\playground\msbuild.ProjectImports.zip' in pipeline
+        /.*\.ProjectImports\.zip/,
+
+        // This stops "react-native run-windows" from causing the metro server to crash if its already running
+        new RegExp(
+          `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+        ),
+      ]),
+    ),
   },
 
   projectRoot: path.resolve(__dirname),
